c6f44a0a0dca0e6e32938b81bded0a4b3ea5bdcd
IAmShubhamSahay <119969435+IAmShubhamSahay@users.noreply.github.com>
2024-08-13 02:28:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 function fibonacci(num) {
-// your code here
-	if(num == 0) return 0;
-	if(num == 1) return 1;
-	let a = 0;
-	let b = 1;
-	for(let i=2; i<= num; i++){
-		let c = a+b;
-		a=b;
-		b=c;
-	}
-	return b;
+    // If num is 0, the first Fibonacci number is 0
+    if (num === 0) return 0;
+    
+    // If num is 1, the first Fibonacci number is 0, so return 0
+    if (num === 1) return 0;
+    
+    let a = 0;
+    let b = 1;
+
+    // Loop from 2 to num to find the nth Fibonacci number
+    for(let i = 2; i < num; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+
+    // If num is 2, return 1
+    return b;
 }
 
 module.exports = fibonacci;
