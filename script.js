4674e84a86a470c06287bede38acdcc674a9d58d
IAmShubhamSahay <119969435+IAmShubhamSahay@users.noreply.github.com>
2024-08-13 02:26:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 function fibonacci(num) {
 // your code here
+	if(num == 0) return 0;
+	if(num == 1) return 1;
 	let a = 0;
 	let b = 1;
-	for(let i=1; i<= num; i++){
+	for(let i=2; i<= num; i++){
 		let c = a+b;
 		a=b;
 		b=c;
 	}
-	return a;
+	return b;
 }
 
 module.exports = fibonacci;
